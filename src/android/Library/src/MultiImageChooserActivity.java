31e2350a1eb1eaa16038ca2a8c41de1cd6ed7a6a
ecordum-care <ecordum-care@users.noreply.github.com>
2017-08-18 23:10:14 +0800

    修改提示为中文

--- src/android/Library/src/MultiImageChooserActivity.java
+++ src/android/Library/src/MultiImageChooserActivity.java
@@ -173,8 +173,8 @@ public class MultiImageChooserActivity extends Activity implements OnItemClickLi
         setupHeader();
         updateAcceptButton();
         progress = new ProgressDialog(this);
-        progress.setTitle("Processing Images");
-        progress.setMessage("This may take a few moments");
+        progress.setTitle("提示");
+        progress.setMessage("正在处理，请稍后");
     }
     
     @Override
